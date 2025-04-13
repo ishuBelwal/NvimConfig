83864d69a5890c27d971081af9268a0dde711d15
ishuBelwal <ishubelwal@gmail.com>
2025-04-13 13:34:43 +0530

    :zap: Mini File Directory

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -7,6 +7,19 @@ return {
 			statusline.setup { use_icons = true }
 			local starter = require 'mini.starter'
 			starter.setup {}
+
+			require('mini.files').setup({
+				windows = {
+					-- Place it on the left like a traditional side explorer
+					position = 'false',
+					width = 30,
+					preview = true, -- optional: shows preview for selected files
+
+				},
+			})
+			vim.keymap.set('n', '<leader>e', function()
+				require('mini.files').open()
+			end, { desc = 'Open file explorer (mini.files)' })
 		end
 	},
 }
